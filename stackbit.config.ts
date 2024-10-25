582708cfbed3817c3084c0a408d806bf2c9c76f7
Christian Parsons <25161410+cmparsons@users.noreply.github.com>
2024-10-25 15:28:37 -0500

    model extensions

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,18 +1,24 @@
-import { ContentfulContentSource } from '@stackbit/cms-contentful';
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig, type SiteMapEntry } from "@stackbit/types";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "astro",
-    "contentSources": [
-        new ContentfulContentSource({
-            spaceId: process.env.CONTENTFUL_SPACE_ID!,
-            environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-            previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!
-        }),
-    ],
-    "postInstallCommand": "npm i --no-save @stackbit/types @stackbit/cms-contentful"
-})
+  stackbitVersion: "~0.6.0",
+  nodeVersion: "18",
+  ssgName: "astro",
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+    }),
+  ],
+  postInstallCommand:
+    "npm i --no-save @stackbit/types @stackbit/cms-contentful",
+  modelExtensions: [
+    { name: "root", type: "page", urlPath: "/" },
+    { name: "book", type: "page", urlPath: "/books/{slug}" },
+    { name: "author", type: "page", urlPath: "/author/{slug}" },
+  ],
+});
