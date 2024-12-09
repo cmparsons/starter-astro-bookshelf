cfd534a311e0b9b7d577aa3eff530e16cc982757
Christian Parsons <25161410+cmparsons@users.noreply.github.com>
2024-12-09 14:39:42 -0600

    comment out site map function

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -6,22 +6,6 @@ export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
   nodeVersion: "18",
   ssgName: 'astro',
-  // ssgName: "custom",
-  // // Astro to run inside Visual Editor container
-  // devCommand: "node_modules/.bin/astro dev --port {PORT} --hostname 127.0.0.1",
-  // // Astro-specific configuration
-  // experimental: {
-  //   ssg: {
-  //     name: "Astro",
-  //     logPatterns: {
-  //       up: ["is ready", "astro"]
-  //     },
-  //     directRoutes: {
-  //       "socket.io": "socket.io"
-  //     },
-  //     passthrough: ["/vite-hmr/**"]
-  //   }
-  // },
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID!,
@@ -34,31 +18,31 @@ export default defineStackbitConfig({
     { name: "bookReferencePage", type: "page" },
     { name: "bookAuthor", type: "page" },
   ],
-  siteMap: ({ documents, models }) => {
-    const pageModels = models
-      .filter((m) => m.type === "page")
+  // siteMap: ({ documents, models }) => {
+  //   const pageModels = models
+  //     .filter((m) => m.type === "page")
 
-    return documents
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      .map((document) => {
-        const urlModel = (() => {
-            switch (document.modelName) {
-                case 'bookReferencePage':
-                    return 'books';
-                case 'bookAuthor':
-                    return 'author';
-                default:
-                    return null;
-            }
-        })();
+  //   return documents
+  //     .filter((d) => pageModels.some(m => m.name === d.modelName))
+  //     .map((document) => {
+  //       const urlModel = (() => {
+  //           switch (document.modelName) {
+  //               case 'bookReferencePage':
+  //                   return 'books';
+  //               case 'bookAuthor':
+  //                   return 'author';
+  //               default:
+  //                   return null;
+  //           }
+  //       })();
 
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  },
+  //       return {
+  //         stableId: document.id,
+  //         urlPath: `/${urlModel}/${document.id}`,
+  //         document,
+  //         isHomePage: false,
+  //       };
+  //     })
+  //     .filter(Boolean) as SiteMapEntry[];
+  // },
 });
